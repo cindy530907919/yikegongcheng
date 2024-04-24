6d21913f39d86422b28f8ad1ea52b6c36573e56d
mizhizhengjing <62551080+mizhizhengjing@users.noreply.github.com>
2024-04-24 11:12:37 +0800

    1

--- yike/pages/index/index.vue
+++ yike/pages/index/index.vue
@@ -1,8 +1,9 @@
 <template>
   <view>
-    <view v-for="user in users" :key="user.id">
+    <!-- <view v-for="user in users" :key="user.id">
       <text>{{ user.name }}</text>
-    </view>
+    </view> -->
+	444
   </view>
 </template>
 
@@ -16,17 +17,10 @@ export default {
     };
   },
   methods: {
-   getindexData(){
-	   const { memberInfo } = await MemberApi.list({ memberId: this.memberInfo.memberId }, true);
-	   //const { memberInfo }后台给我数组
-	   // { memberId: this.memberInfo.memberId }  我给后台传的数据
-   },
-   goodsList(){
-	   const {goodsList } = await MemberApi.goodsList({ memberId: this.memberInfo.memberId }, true);
-   }
+   
   },
   mounted() {
-    this.getindexData(); // 组件加载时获取数据
+    
   }
 };
 </script>

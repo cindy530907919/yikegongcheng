5f99b0cfadc8ad7d1a0e88a40c8b45767de02705
南海 <15590687286@163.com>
2024-05-15 15:15:30 +0800

    登录

--- yike/pages/login/login.vue
+++ yike/pages/login/login.vue
@@ -8,12 +8,12 @@
 		<view class="clear"></view>
 		<navigator class="loginWechat">
 			<image src="../../static/images/wechat_b.png" mode="widthFix" class="loginWechatIcon"></image>
-			<text>微信登录11</text>
+			<text>微信登录</text>
 		</navigator>
 		<label class="loginRadio">
 			<checkbox checked="true" class="loginRadioC" value="1" checked-color="#FF5722" />
 			<view class="loginRadioText">
-				同 意
+				同意
 				<navigator url="">用户服务协议</navigator>
 				和
 				<navigator url="">隐私协议</navigator>
